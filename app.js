0adbb3a781a2b0f43d9223b826385cef31b5b75a
Romain <romain.nicaise@fleet-performance.com>
2017-08-31 15:55:12 +0200

    seconde commit

--- app.js
+++ app.js
@@ -4,11 +4,9 @@
 //---------------------------------------------------------------------
 
 var Bleacon = require('bleacon');
-
 var moment = require('moment')
 var _ = require('underscore')
 var mqtt = require('mqtt')
-
 var serialport = require('serialport');
 
 var port = new serialport('/dev/ttyO1', {
@@ -24,7 +22,7 @@ var gps = new GPS;
 // UPDATE GPS DATA
 //---------------------------------------------------------------------
 
-var RTCReady = true;
+var RTCReady = false;
 var MQTTGPSMessage = [];
 var gpsTime = 'null'
 var lat = 'null'
@@ -39,6 +37,8 @@ A ce moment le flag RTCReady est en mode true et le reste du programme peut comm
 */
 
 gps.on('data', function (data) {
+
+  // -> TRAMES GPS
   //console.log(data);
 
   /*
@@ -46,8 +46,7 @@ gps.on('data', function (data) {
   */
 
   if (data.type == 'GSV') {
-    //console.log(data)
-    //console.log((data.satellites).length)
+
     if ((data.satellites).length > 1 && RTCReady == false) {
       RTCReady = true
       console.log("RTC is ready. Starting acquisition")
@@ -55,20 +54,15 @@ gps.on('data', function (data) {
   }
 
   if (data.type == 'RMC' && RTCReady == true) {
-    //console.log(data)
+
     gpsTime = String(moment(Date.parse(data.time), 'x').format())
-    //console.log(gpsTime)
 
     if (data.lat !== null && data.lon !== null) {
-
-      //console.log("Acquiring GPS position from RMC message")
 
       MQTTGPSMessage.push({ gpsTime: gpsTime, lat: data.lat, lng: data.lon })
 
     }
-
   }
-
 });
 
 port.on('data', function (data) {
@@ -87,22 +81,15 @@ var targetDuration = 2000;
 var MQTTStickerMessage = [];
 var sitckerInterval = 4000;
 
-
-
-
-//console.log("test")
-
 var stickers =
   [
-    { id: "1", uuid: "d0d3fa86ca7645ec9bd96af4f7e68036" , id: "f7e68036389dfca6", type: "shoes", receivedMessages: 0, begin: "", end: "", duration: 0 },
-    { id: "2", uuid: "d0d3fa86ca7645ec9bd96af47c040c32" , id: "7c040c329d1c6390", type: "door", receivedMessages: 0, begin: "", end: "", duration: 0 },
-    { id: "3", uuid: "d0d3fa86ca7645ec9bd96af45f05d0b4" , id: "5f05d0b42fcd88ca", type: "bag", receivedMessages: 0, begin: "", end: "", duration: 0 },
-    { id: "4", uuid : "d0d3fa86ca7645ec9bd96af46c3229e7", id: "6c3229e76c391f79", type: "bed", receivedMessages: 0, begin: "", end: "", duration: 0 },
-    { id: "5", uuid : "b9407f30f5f8466eaff925556b57fe7d", id: "", type: "blueberry", receivedMessages: 0, begin: "", end: "", duration: 0 },
-    { id: "6", uuid : "b9407f30f5f8466eaff925556b570080", id: "", type: "green", receivedMessages: 0, begin: "", end: "", duration: 0 }
+    { id: "1", uuid: "d0d3fa86ca7645ec9bd96af4f7e68036", id: "f7e68036389dfca6", type: "shoes", receivedMessages: 0, begin: "", end: "", duration: 0 },
+    { id: "2", uuid: "d0d3fa86ca7645ec9bd96af47c040c32", id: "7c040c329d1c6390", type: "door", receivedMessages: 0, begin: "", end: "", duration: 0 },
+    { id: "3", uuid: "d0d3fa86ca7645ec9bd96af45f05d0b4", id: "5f05d0b42fcd88ca", type: "bag", receivedMessages: 0, begin: "", end: "", duration: 0 },
+    { id: "4", uuid: "d0d3fa86ca7645ec9bd96af46c3229e7", id: "6c3229e76c391f79", type: "bed", receivedMessages: 0, begin: "", end: "", duration: 0 },
+    { id: "5", uuid: "b9407f30f5f8466eaff925556b57fe7d", id: "", type: "blueberry", receivedMessages: 0, begin: "", end: "", duration: 0 },
+    { id: "6", uuid: "b9407f30f5f8466eaff925556b570080", id: "", type: "green", receivedMessages: 0, begin: "", end: "", duration: 0 }
   ]
-
-
 
 /* 
  
@@ -112,8 +99,6 @@ Si une association est validée on incrémente la durée de x sur le beacon
 Une fois l'association finir, si celle ci a une durée suffisante - targetDuration - la durée totale d'association est enregistrée et envoyée sur le topic MQTT
  
 */
-
-
 
 setInterval(function () {
 
@@ -136,20 +121,14 @@ setInterval(function () {
           MQTTStickerMessage.push({ beacon: el.type, event: "presence", begin: el.begin, end: String(moment(gpsTime).subtract(sitckerInterval / 1000, 'seconds').format()), duration: el.duration })
 
         }
-
         el.duration = 0
         el.begin = gpsTime
       }
-
 
       el.receivedMessages = 0
     })
 
   }
-
-  
-
-
 
 }, sitckerInterval);
 
@@ -161,10 +140,6 @@ setInterval(function () {
 
 Bleacon.on('discover', function (estimoteSticker) {
   //console.log(estimoteSticker);
-
-  //console.log("packet at " + String(moment().format('MMMM Do YYYY, h:mm:ss a')) + " for ID: " + String(estimoteSticker.id) + "acc :" + String(estimoteSticker.acceleration.z))
-
-  //console.log(_.filter(stickers, function (el) { return el.uiid == estimoteSticker.id })[0])
 
   var object = (_.filter(stickers, function (el) { return el.uuid == estimoteSticker.uuid })[0])
 
@@ -178,22 +153,13 @@ Bleacon.on('discover', function (estimoteSticker) {
       }
     })
   }
-
-
-
 });
 
 Bleacon.startScanning();
 
-
-
-
-
-
 //---------------------------------------------------------------------
 // TRANSFER PROCESS VIA MQTT OF STICKER OBJECT
 //---------------------------------------------------------------------
-
 
 var client = mqtt.connect({ host: 'localhost', port: 1883 })
 
@@ -209,8 +175,6 @@ client.on('connect', function () {
       MQTTGPSMessage = []
     }
 
-
-
     if (MQTTStickerMessage.length > 0) {
 
       _.map(MQTTStickerMessage, function (el) {
@@ -223,10 +187,6 @@ client.on('connect', function () {
       MQTTStickerMessage = []
 
     }
-
   }, 5000);
-
-
-
 
 })
